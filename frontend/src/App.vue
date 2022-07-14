ba3d3232fce0f7dda509e97d7a85349c4c1fd774
Marcio Antunes <marcioantunes.ma@gmail.com>
2022-07-14 12:20:00 -0300

    Frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,109 +1,65 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-</script>
- 
- <!--
- <template>
-
-    <div class="page">
-      
-        <div class="card">
-            <div class="card-header">
-
-                <header>
-
-                    <span class="float-left">
-                        <RouterLink to="/tarefas">Tarefas</RouterLink>
-                        <RouterLink to="/historico">Histórico</RouterLink>
-                    </span>
-                    <span class="float-right">
-                        <i class="fa fa-search" id='icon' aria-hidden="true"></i>
-                    </span>
-                </header>
-            </div>
-
-            <div class="card-body">
-                <RouterView />
-
-            </div>
-        </div>
-
-    </div>
--->
 <template>
-    <div id="app" >
+    <div id="app">
         <div class="card">
             <div class="card-header">
-
                 <header>
-
                     <span class="float-left">
-                        <RouterLink to="/tarefas">Tarefas</RouterLink>
-                        <RouterLink to="/historico">Histórico</RouterLink>
+                        <router-link to="/tasks">Tasks</router-link>
+                        <router-link to="/tarefas">Tarefas</router-link>
+                        <router-link to="/historico">Histórico</router-link>
                     </span>
                     <span class="float-right">
-                        <i class="fa fa-search" id='icon' aria-hidden="true"></i>
+                        <i
+                            class="fa fa-search"
+                            id="icon"
+                            aria-hidden="true"
+                        ></i>
                     </span>
                 </header>
             </div>
 
             <div class="card-body">
-                <RouterView />
-
+                <router-view />
             </div>
         </div>
-
     </div>
 </template>
 <script>
 export default {
-    name: 'App',
+    name: "App",
+};
+</script>
+
+<style>
+body {
+    background-color: rgba(165, 159, 151, 0.425);
 }
 
+a {
+    padding: 10px;
+}
 
-</script>
-               
-     
-         
-            <style>
-            body {
-                background-color: rgba(165, 159, 151, 0.425);
-            }
-            
-            a {
-            
-                padding: 10px;
-            }
-            
-            #icon {
-                padding: 7px;
-                margin-right: 10px;
-                margin-bottom: 8px;
-            }
-            
-            
-            .card {
-                width: 90%;
-                margin: auto;
-            }
-            
-            .page {
-            
-                margin-top: 50px;
-                margin-bottom: 50px;
-            }
-            
-            
-            
-            .float-left {
-            
-            
-                font-size: 23px;
-            
-            }
-            
-            .card-body {
-                margin: 1%;
-            }
-            </style>
-      
+#icon {
+    padding: 7px;
+    margin-right: 10px;
+    margin-bottom: 8px;
+}
+
+.card {
+    width: 90%;
+    margin: auto;
+}
+
+.page {
+    margin-top: 50px;
+    margin-bottom: 50px;
+}
+
+.float-left {
+    font-size: 23px;
+}
+
+.card-body {
+    margin: 1%;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,27 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import tarefasv from '../views/tarefasv.vue';
-import historicov from '../views/historicov.vue'; 
-import forms from '../views/forms.vue'; 
+import { createRouter, createWebHistory } from "vue-router";
+import tarefasv from "../views/tarefasv.vue";
+import historicov from "../views/historicov.vue";
+import forms from "../views/forms.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/tarefas',
-      name: 'tarefas',
-      component: tarefasv
-    }
-    , {
-      path: '/historico',
-      name: 'historico',
-      component: historicov
-    },
-    {
-      path:'/criar',
-      name: 'criar',
-      component: forms
-    }
-  ]
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/tarefas",
+            name: "tarefas",
+            component: tarefasv,
+        },
+        {
+            path: "/historico",
+            name: "historico",
+            component: historicov,
+        },
+        {
+            path: "/criar",
+            name: "criar",
+            component: forms,
+        },
+        {
+            path: "/tasks",
+            name: "Tasks",
+            component: () => import("../views/Tasks/Index.vue"),
+        },
+    ],
+});
 
-export default router
+export default router;
